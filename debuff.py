12b819283415d4a3803e58be3aa0c7a02752e118
Marcelo <mvrozanti@hotmail.com>
2018-02-13 13:45:31 -0200

    removed scrape

--- debuff.py
+++ debuff.py
@@ -11,26 +11,25 @@ con = sqlite3.connect('dotabuff.sqlite')
 def list_heroes():
     res = sess.get('https://www.dotabuff.com/heroes').text
     bs = BeautifulSoup(res,'lxml')
-    as_ = bs.find_all(lambda tag: tag.name == 'a' and tag.has_attr('href') and '/heroes/' in tag.get('href'))[10:] # first 10 results are worthless atm
+    as_ = bs.find_all(lambda tag: tag.name == 'a' \
+            and tag.has_attr('href') \
+            and '/heroes/' in tag.get('href'))[10:] # first 10 results are worthless atm
     return [a.get('href').replace('/heroes/','') for a in list(filter(None, as_))]
 
 def parse_hero_page(hero):
-    url = 'https://www.dotabuff.com/heroes/' + hero + '/matchups'
-    bs = BeautifulSoup(sess.get(url).text, 'lxml')
-    lines = bs.find_all(lambda tag: tag.name == 'tr' and tag.has_attr('data-link-to'))
-    i = 0
-    for l in lines:
-        other_hero = l.get('data-link-to').replace('/heroes/','')
-        tds = l.find_all(lambda tag: tag.name == 'td')
-        advtg = float(tds[2].text[:-1])
-        con.execute('INSERT OR REPLACE INTO HERO VALUES (?,?,?)', [hero, advtg, other_hero])
-        i+=1
-        if not i % len(heroes) - 2: con.commit()
+    bs = BeautifulSoup(sess.get('https://www.dotabuff.com/heroes/' + 
+        hero + '/matchups').text, 'lxml')
+    lines = bs.find_all(lambda tag: tag.name == 'tr' \
+            and tag.has_attr('data-link-to'))
+    for i in range(len(lines)):
+        advtg = float(lines[i].find_all(lambda tag: tag.name == 'td')[2].text[:-1])
+        con.execute('INSERT OR REPLACE INTO HERO VALUES (?,?,?)',\
+                [hero, advtg, lines[i].get('data-link-to').replace('/heroes/','')])
+        if not i % len(heroes) - 2: con.commit() # do not trash rest of computer
     con.commit()
 
 def update_advantages():
-    heroes = list_heroes()
-    for h in heroes: parse_hero_page(h)
+    for h in list_heroes(): parse_hero_page(h)
 
 def get_counters_for(heroes):
     hero_group = str(heroes).replace('"','\'').replace(']',')').replace('[','(')
