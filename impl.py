2adb1ea7868d3763f98f4178b2462c919274f9ef
Nexor <mvrozanti@hotmail.com>
2018-07-28 16:37:43 -0300

    added -q parameter

--- impl.py
+++ impl.py
@@ -15,7 +15,8 @@ group       = parser.add_mutually_exclusive_group(required=True)
 group.add_argument('-s', action='store_true', help='smart auto insert from oldest in record to older')
 group.add_argument('-n', action='store_true', help='auto insert from newest in record to newest')
 group.add_argument('-b', action='store_true', help='both [WIP]')
-parser.add_argument('-v', action='store_true', help='verbose')
+parser.add_argument('-v', default=1, help='verbose')
+parser.add_argument('-q', action='store_true', help='query picks')
 parser.add_argument('-c',metavar='request-count', type=int, default=2,help='amount of concurrent requests (default=2)')
 # parser.add_argument('-o',metavar='output-file',default='addresses.txt',help='file to output tested working ips')
 parsed = parser.parse_args()
@@ -59,9 +60,8 @@ def insert_match(match_id):
         if len(heroes) != 10: raise Exception('Incomplete match')
         radiant_heroes = heroes[:5]
         radiant_win = match['radiant_win']
-        for h in heroes:
-            cur.execute('INSERT OR IGNORE INTO match VALUES (?,?,?,?)', \
-                    [match_id, radiant_win, h, h in radiant_heroes])
+        match = [[match_id, radiant_win, h, h in radiant_heroes] for h in heroes]
+        cur.executemany('INSERT OR IGNORE INTO match VALUES (?,?,?,?)', match)
         con.commit()
         LOG.debug(str(match_id) + '\tOK') 
     except Exception as e:
@@ -77,8 +77,29 @@ def scrape(incremental=True):
     insert_match(newest_match if incremental else oldest_match)
     scrape(incremental=incremental)
 
+def best_picks_having(to_counter, to_combo):
+    query = """
+            SELECT hero, COUNT(*)
+            FROM   match
+            WHERE  match_id IN (SELECT match_id
+                                FROM   match
+                                WHERE  (radiant_win = 1 AND hero IN (?))
+                                       OR (radiant_win = 0 AND hero IN (?))
+                                GROUP BY match_id)
+                   AND hero NOT IN (?)
+                   AND radiant_win = radiant_team 
+            GROUP BY hero
+            ORDER BY COUNT(*) ASC;
+            """
+    best_picks = cur.execute(query, (','.join(to_counter),','.join(to_combo), ','.join(to_counter+to_combo))).fetchall()
+    for p in best_picks: print(p)
+
+if parsed.q:
+    best_picks_having(['Weaver'],[])
+    sys.exit(0)
+
 if parsed.s or parsed.b: 
-    for _ in range(parsed.c//2): threading.Thread(target=scrape, kwargs=dict(incremental=False)).start()
+    for _ in range(parsed.c//2 if parsed.b else parsed.c): threading.Thread(target=scrape, kwargs=dict(incremental=False)).start()
 
 if parsed.n or parsed.b: 
-    for _ in range(parsed.c//2): threading.Thread(target=scrape, kwargs=dict(incremental=True)).start()
+    for _ in range(parsed.c//2 if parsed.b else parsed.c): threading.Thread(target=scrape, kwargs=dict(incremental=True)).start()
